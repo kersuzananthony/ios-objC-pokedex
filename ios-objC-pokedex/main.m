4a2c34a28fc00f163f572c690857d2951d9d34a1
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-03-28 12:41:24 +0800

    Initial Commit

---
+++ ios-objC-pokedex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ios-objC-pokedex
+//
+//  Created by Kersuzan on 28/03/16.
+//  Copyright © 2016 Kersuzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
